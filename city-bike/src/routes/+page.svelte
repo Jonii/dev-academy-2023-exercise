cf9da4b0a2d11f24c7c639e7b1193cec4ecc978a
Jph <2147702+Jonii@users.noreply.github.com>
2023-04-15 13:21:33 +0300

    Add license to page

--- city-bike/src/routes/+page.svelte
+++ city-bike/src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+Trip and station data from
+https://www.hsl.fi/en/hsl/open-data
+https://www.avoindata.fi/data/en_GB/dataset/hsl-n-kaupunkipyoraasemat/resource/a23eef3a-cc40-4608-8aa2-c730d17e8902
