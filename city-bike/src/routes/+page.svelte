d7b7036b994ce1bdbea795facfb05e5a253585d0
Jph <2147702+Jonii@users.noreply.github.com>
2023-04-15 13:25:36 +0300

    Add a bit of styling to license layout

--- city-bike/src/routes/+page.svelte
+++ city-bike/src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Joniis bike trip page</h1>
+<p>Here we have bike stuff</p>
 
 Trip and station data from
-https://www.hsl.fi/en/hsl/open-data
-https://www.avoindata.fi/data/en_GB/dataset/hsl-n-kaupunkipyoraasemat/resource/a23eef3a-cc40-4608-8aa2-c730d17e8902
+<p><a href="https://www.hsl.fi/en/hsl/open-data">HSL Open data</a></p>
+<p><a href="https://www.avoindata.fi/data/en_GB/dataset/hsl-n-kaupunkipyoraasemat/resource/a23eef3a-cc40-4608-8aa2-c730d17e8902">Avoin data for bike stations</a></p>
