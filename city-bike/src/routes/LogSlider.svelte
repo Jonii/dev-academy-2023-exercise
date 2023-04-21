be1e1205245c97b6b8e33b54ce0646495b9a723d
Jph <2147702+Jonii@users.noreply.github.com>
2023-04-21 13:19:09 +0300

    Slider now re-centers on the spot you release it at.

--- city-bike/src/routes/LogSlider.svelte
+++ city-bike/src/routes/LogSlider.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-    let centerValue = 50;
+    const trueCenter = 50;
+    let centerValue = trueCenter;
     let sliderValue = centerValue;
     let min = 0;
     let max = 100;
     export let valueToUnit = _valueToUnit;
     export let selectedValue: Date = new Date();
+    let sliderElement: HTMLElement;
     let dateSelectorDisplayVal = new Date();
     const timeRadius = 1000 * 60 * 60 * 12;
+
+    $: sliderElement && sliderElement.style.setProperty('--highlight-center', `${sliderValue}%`);
+    $: sliderElement && console.log(sliderElement.style.getPropertyValue("--highlight-center"));
 
     function _valueToUnit(basevalue: Date, rawUnitless: number): Date {
         timeRadius;        
@@ -32,6 +37,8 @@
     
     function resetSlider() {
         selectedValue.setTime(dateSelectorDisplayVal.getTime());
+        centerValue = Math.round((sliderValue * 2 + centerValue) / 3);
+        
         sliderValue = centerValue;
     }
 </script>
@@ -44,11 +51,62 @@
         {max}
         on:input={() => updateViewValue(selectedValue)}
         on:change={resetSlider}
+        bind:this={sliderElement}
     />
-    <p>Slider value: {sliderValue}</p>
+    <!--
+        <p>Slider value: {sliderValue}</p>
+    -->
 
     <p>
         Selected time: <span>{dateSelectorDisplayVal.toLocaleTimeString()}</span>
         <span style="font-size: 0.8rem">{dateSelectorDisplayVal.toLocaleDateString("fi-Fi")}</span>
     </p>
 </div>
+
+<style>
+    :root {
+        --thumb-height: 35px;
+        --thumb-width: 40px;
+        --highlight-center: 50%;
+    }
+
+    input[type=range] {
+        -webkit-appearance: none;
+        height: 35px; 
+        padding: 0;	
+    }
+
+    input[type=range]::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        box-sizing: content-box;
+        width: var(--thumb-width);
+        height: var(--thumb-height);
+    }
+
+    input[type=range]::-moz-range-thumb {
+        width: var(--thumb-width);
+        height: var(--thumb-height);
+    }
+
+    input[type=range]::-ms-thumb {
+        width: var(--thumb-width);
+        height: var(--thumb-height);
+    }
+
+    input[type=range]::-webkit-slider-runnable-track {
+        background: #367ebd;
+    }
+
+    input[type=range]::-moz-range-track {
+        width: 100%;
+        height: 5px;
+        cursor: pointer;
+        background: radial-gradient(circle at var(--highlight-center), #fadfac 20%, #0be930 100%);
+        border: 1px outset black;
+    }
+
+    input[type=range]::-ms-track { 
+        border-color: transparent;
+        color: transparent;
+    }
+</style>
